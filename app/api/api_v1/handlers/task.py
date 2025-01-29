2a01f06f7e286a108f5e710154c615cdba0ac44e
Matheus Vidal <matheusvidal140@gmail.com>
2025-01-29 14:37:11 -0300

    Finalizando o CRUD de tasks

--- app/api/api_v1/handlers/task.py
+++ app/api/api_v1/handlers/task.py
@@ -1,22 +1,46 @@
-from fastapi import APIRouter, Depends
-from schemas.task_schema import TaskDetail, TaskCreate
+from fastapi import APIRouter, Depends, status, Response
+from schemas.task_schema import TaskDetail, TaskCreate, TaskUpdate
 from models.user_model import User
 from models.task_model import Task
 from api.dependencies.user_deps import get_current_user
 from typing import List
 from services.task_service import TaskService
+from uuid import UUID
 
 task_router = APIRouter()
 
-@task_router.get("/",
+@task_router.get('/',
                 summary='Get all tasks for the current user',
-                response_model=List[TaskDetail])
-async def list_tasks(user: User = Depends(get_current_user)):
+                response_model=List[TaskDetail]
+                )
+async def list(user: User = Depends(get_current_user)):
   return await TaskService.list_tasks(user)
 
-@task_router.post("/",
+@task_router.get('/{task_id}',
+                 summary='Get a task by ID',
+                 response_model=TaskDetail
+                )
+async def get(task_id: UUID, user: User = Depends(get_current_user)):
+  return await TaskService.get_task(task_id, user)
+
+@task_router.post('/',
                 summary='Create a new task',
                 response_model=Task
                 )
-async def create_task(data: TaskCreate, user: User = Depends(get_current_user)):
+async def create(data: TaskCreate, user: User = Depends(get_current_user)):
   return await TaskService.create_task(data, user)
+
+@task_router.put('/{task_id}',
+                summary='Update a task by ID',
+                response_model=Task
+                )
+async def update(task_id: UUID, data: TaskUpdate, user: User = Depends(get_current_user)):
+  return await TaskService.update_task(task_id, data, user)
+
+@task_router.delete('/{task_id}',
+                    summary='Delete a task by ID',
+                    response_model=Task
+                    )
+async def delete(task_id: UUID, user: User = Depends(get_current_user)):
+  await TaskService.delete_task(task_id, user)
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/services/task_service.py
+++ app/services/task_service.py
@@ -1,7 +1,8 @@
 from models.user_model import User
 from models.task_model import Task
 from typing import List
-from schemas.task_schema import TaskCreate
+from schemas.task_schema import TaskCreate, TaskUpdate
+from uuid import UUID
 
 class TaskService:
   @staticmethod
@@ -13,3 +14,22 @@ class TaskService:
     task = Task(**data.dict(), owner=user)
     return await task.insert()
   
+  @staticmethod
+  async def get_task(task_id: UUID, user: User) -> Task:
+    return await Task.find_one(Task.owner.id == user.id, Task.task_id == task_id)
+  
+  @staticmethod
+  async def update_task(task_id: UUID, data: TaskUpdate, user: User) -> Task:
+    task = await TaskService.get_task(task_id, user)
+    if task:
+      update_data = data.dict(exclude_unset=True)
+      await task.update({'$set': update_data})
+      return await task.save()
+    return None
+  
+  @staticmethod
+  async def delete_task(task_id: UUID, user: User) -> None:
+    task = await TaskService.get_task(task_id, user)
+    if task:
+      await task.delete()
+    return None
